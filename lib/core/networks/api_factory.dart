9cdc51f6d044c391855fd165530b067d70401040
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-21 05:21:53 +0300

    added api factory ot networks folder

---
+++ lib/core/networks/api_factory.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ApiFactory {
+  FirebaseAuth? firebaseAuth;
+  FirebaseFirestore? firestore;
+
+  // FirebaseFactory() {
+  //   firebaseAuth ??= FirebaseAuth.instance;
+
+  //   firestore ??= FirebaseFirestore.instance;
+  // }
+}
