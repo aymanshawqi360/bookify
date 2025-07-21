e6153bbae9439f711842381348525bda0c2f3f6d
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-21 10:39:54 +0300

    added amendment on ap text button folder

--- lib/core/widget/app_text_button.dart
+++ lib/core/widget/app_text_button.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:book_rental_app/core/themes/color_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:book_rental_app/core/themes/styles.dart';
 import 'package:book_rental_app/core/utils/extensions.dart';
 import 'package:book_rental_app/core/utils/hex_colors.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTextButton extends StatelessWidget {
   final double? width;
@@ -12,16 +15,18 @@ class AppTextButton extends StatelessWidget {
   final void Function()? onTap;
   final TextStyle? textStyles;
   final BorderRadiusGeometry? borderRadius;
+  final bool? isLoading;
   const AppTextButton({
+    Key? key,
     this.width,
     this.height,
     this.decoration,
     this.text,
     this.onTap,
     this.textStyles,
-    super.key,
     this.borderRadius,
-  });
+    this.isLoading,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -39,10 +44,20 @@ class AppTextButton extends StatelessWidget {
               borderRadius: borderRadius ?? BorderRadius.circular(19.r),
             ),
         child: Center(
-          child: Text(
-            text ?? "LogIn",
-            style: textStyles ?? TextStyles.fon14WhiteMedium,
-          ),
+          child:
+              (isLoading == true)
+                  ? Transform.scale(
+                    scale: 0.6,
+
+                    child: CircularProgressIndicator(
+                      strokeWidth: 5,
+                      color: ColorsManager.white,
+                    ),
+                  )
+                  : Text(
+                    text ?? "LogIn",
+                    style: textStyles ?? TextStyles.fon14WhiteMedium,
+                  ),
         ),
       ),
     );
