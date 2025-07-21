263661dcc7bed3ee6b50ade3ecce3d4cb9046447
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-21 23:09:19 +0300

    added amendment on styles

--- lib/core/themes/styles.dart
+++ lib/core/themes/styles.dart
@@ -2,7 +2,6 @@ import 'package:book_rental_app/core/config/app_strings.dart';
 import 'package:book_rental_app/core/themes/color_manager.dart';
 import 'package:book_rental_app/core/themes/font_weight_helper.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyles {
   static const TextStyle font32BlackSemiBold = TextStyle(
@@ -40,5 +39,17 @@ class TextStyles {
     fontSize: 14,
     // fontFamily: AppStrings.fonFamilyApple,
     fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle fon12GrayMedium = TextStyle(
+    color: ColorsManager.gray,
+    fontSize: 12,
+    // fontFamily: AppStrings.fonFamilyApple,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle fon12BlueSemiBold = TextStyle(
+    color: ColorsManager.blue,
+    fontSize: 12,
+    // fontFamily: AppStrings.fonFamilyApple,
+    fontWeight: FontWeightHelper.semiBold,
   );
 }
