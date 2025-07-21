e6c9ce07cb56e86ed3276e01e00985faa21687ec
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-21 23:07:18 +0300

    added amendment on app_router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:book_rental_app/core/di/dependency_injection.dart';
 import 'package:book_rental_app/core/routes/routes.dart';
-import 'package:book_rental_app/features/mobile/auth/presentation/cubit/login_cubit.dart';
-import 'package:book_rental_app/features/mobile/auth/presentation/screen/login_screen.dart';
+import 'package:book_rental_app/features/mobile/auth/login/presentation/cubit/login_cubit.dart';
+import 'package:book_rental_app/features/mobile/auth/login/presentation/screen/login_screen.dart';
+import 'package:book_rental_app/features/mobile/auth/sign_up/presentation/cubit/cubit/sign_up_cubit.dart';
+import 'package:book_rental_app/features/mobile/auth/sign_up/presentation/screen/sign_up_screen.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,6 +17,14 @@ class AppRouter {
               (_) => BlocProvider(
                 create: (context) => sl<LoginCubit>(),
                 child: LoginScreen(),
+              ),
+        );
+      case Routes.authSignUp:
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider(
+                create: (context) => sl<SignUpCubit>(),
+                child: SignUpScreen(),
               ),
         );
 
