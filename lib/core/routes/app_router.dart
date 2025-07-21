429ff02436499f095f885e846c6a533f7913b8ab
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-21 10:39:30 +0300

    added amendment on app router folder

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,13 +1,22 @@
+import 'package:book_rental_app/core/di/dependency_injection.dart';
 import 'package:book_rental_app/core/routes/routes.dart';
+import 'package:book_rental_app/features/mobile/auth/presentation/cubit/login_cubit.dart';
 import 'package:book_rental_app/features/mobile/auth/presentation/screen/login_screen.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case Routes.authLogin:
-        return MaterialPageRoute(builder: (_) => LoginScreen());
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider(
+                create: (context) => sl<LoginCubit>(),
+                child: LoginScreen(),
+              ),
+        );
 
       default:
         null;
