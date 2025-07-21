595f493d3fec376bd2d12e35760612dbf311282f
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-21 23:06:39 +0300

    added amendment on dependency_injection

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,8 +1,14 @@
-import 'package:book_rental_app/features/mobile/auth/data/api/login_api_service.dart';
-import 'package:book_rental_app/features/mobile/auth/data/repo_implementation/repo_implemenation.dart';
-import 'package:book_rental_app/features/mobile/auth/domain/login_use_cases.dart';
-import 'package:book_rental_app/features/mobile/auth/domain/repo/login_repo_domain.dart';
-import 'package:book_rental_app/features/mobile/auth/presentation/cubit/login_cubit.dart';
+import 'package:book_rental_app/core/networks/api_factory.dart';
+import 'package:book_rental_app/features/mobile/auth/login/data/api/login_api_service.dart';
+import 'package:book_rental_app/features/mobile/auth/login/data/repo_implementation/repo_implemenation.dart';
+import 'package:book_rental_app/features/mobile/auth/login/domain/login_use_cases.dart';
+import 'package:book_rental_app/features/mobile/auth/login/domain/repo/login_repo_domain.dart';
+import 'package:book_rental_app/features/mobile/auth/login/presentation/cubit/login_cubit.dart';
+import 'package:book_rental_app/features/mobile/auth/sign_up/data/api/sign_up_api_service.dart';
+import 'package:book_rental_app/features/mobile/auth/sign_up/data/repo_implementation/sign_up_repo_implementaion.dart';
+import 'package:book_rental_app/features/mobile/auth/sign_up/domain/repo/sign_up_repo_domain.dart';
+import 'package:book_rental_app/features/mobile/auth/sign_up/domain/use_cases/sign_up_use_cases.dart';
+import 'package:book_rental_app/features/mobile/auth/sign_up/presentation/cubit/cubit/sign_up_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -12,17 +18,25 @@ void setup() {
 
   //Bloc
   sl.registerFactory(() => LoginCubit(sl()));
+  sl.registerFactory(() => SignUpCubit(sl()));
 
   // Data Layer
   sl.registerLazySingleton<LoginApiService>(() => LoginApiService());
+  sl.registerLazySingleton<SignUpApiService>(() => SignUpApiService());
 
   //Use Cases
   sl.registerLazySingleton<LoginUseCases>(
     () => LoginUseCases(loginRepoDomain: sl()),
   );
+  sl.registerLazySingleton<SignUpUseCases>(
+    () => SignUpUseCases(signUpRespo: sl()),
+  );
 
   sl.registerLazySingleton<LoginRepoDomain>(
     () => RepoImplemenation(apiFactory: sl()),
+  );
+  sl.registerLazySingleton<SignUpRepoDomain>(
+    () => SignUpRepoImplementaion(signUpService: sl()),
   );
 
   //!Core
