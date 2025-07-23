8b2bafcc5e48150f5d0e70d6dbfbc8c8addafc51
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-23 22:46:48 +0300

    added amendment in api_factory

--- lib/core/networks/api_factory.dart
+++ lib/core/networks/api_factory.dart
@@ -1,13 +1,47 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-class ApiFactory {
-  FirebaseAuth? firebaseAuth;
-  FirebaseFirestore? firestore;
-
-  // FirebaseFactory() {
-  //   firebaseAuth ??= FirebaseAuth.instance;
-
-  //   firestore ??= FirebaseFirestore.instance;
+class DioFactory {
+  DioFactory._();
+  static Dio? dio;
+  // DioFactory() {
+  //   Duration time = const Duration(seconds: 30);
+  //   if (dio == null) {
+  //     dio = Dio();
+  //     dio!
+  //       ..options.receiveTimeout = time
+  //       ..options.connectTimeout = time;
+  //     getInterceptors();
+  //     dio!;
+  //   } else {
+  //     dio!;
+  //   }
   // }
+
+  static dioInit() {
+    Duration time = const Duration(seconds: 30);
+    if (dio == null) {
+      dio = Dio();
+      dio!
+        ..options.receiveTimeout = time
+        ..options.connectTimeout = time;
+      getInterceptors();
+      return dio!;
+    } else {
+      return dio!;
+    }
+  }
+
+  static void getInterceptors() {
+    dio?.interceptors.add(
+      PrettyDioLogger(
+        requestBody: true,
+        request: true,
+        requestHeader: true,
+        responseBody: true,
+        responseHeader: true,
+        error: true,
+      ),
+    );
+  }
 }
