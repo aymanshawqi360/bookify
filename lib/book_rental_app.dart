9f18f3377325215a820c78e2e678fccc14a041f1
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-23 22:54:41 +0300

    added amendment in book_rental_app

--- lib/book_rental_app.dart
+++ lib/book_rental_app.dart
@@ -1,5 +1,10 @@
+import 'package:book_rental_app/core/di/dependency_injection.dart' show sl;
+import 'package:book_rental_app/core/networks/api_factory.dart';
 import 'package:book_rental_app/core/routes/app_router.dart';
+import 'package:book_rental_app/core/routes/routes.dart';
 import 'package:book_rental_app/core/themes/color_manager.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:device_preview/device_preview.dart';
@@ -19,9 +24,14 @@ class BookRentalApp extends StatelessWidget {
           useInheritedMediaQuery: true,
           locale: DevicePreview.locale(context),
           builder: DevicePreview.appBuilder,
-          theme: ThemeData.light(),
-          darkTheme: ThemeData.dark(),
+          theme: ThemeData(scaffoldBackgroundColor: ColorsManager.white),
+          // darkTheme: ThemeData.dark(),
           debugShowCheckedModeBanner: false,
+          initialRoute:
+              (FirebaseAuth.instance.currentUser != null &&
+                      FirebaseAuth.instance.currentUser!.emailVerified)
+                  ? Routes.homeScreen
+                  : Routes.authLogin,
           onGenerateRoute: _appRouter.generateRoute,
         );
       },
