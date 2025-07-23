28d0d1ecc011e250119a2aff5cfb9246a0289827
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-23 22:53:22 +0300

    added amendment in sign_up_repo_implementaion

--- lib/features/mobile/auth/sign_up/data/repo_implementation/sign_up_repo_implementaion.dart
+++ lib/features/mobile/auth/sign_up/data/repo_implementation/sign_up_repo_implementaion.dart
@@ -1,13 +1,14 @@
 import 'package:book_rental_app/core/errors/api_error_hundler.dart';
 import 'package:book_rental_app/core/errors/api_resulte.dart';
-import 'package:book_rental_app/features/mobile/auth/sign_up/data/api/sign_up_api_service.dart';
 import 'package:book_rental_app/features/mobile/auth/sign_up/data/mapper/sign_up_mapper.dart';
 import 'package:book_rental_app/features/mobile/auth/sign_up/domain/entity/sign_up_request_body_entity.dart';
 import 'package:book_rental_app/features/mobile/auth/sign_up/domain/entity/sign_up_response_model_entity.dart';
 import 'package:book_rental_app/features/mobile/auth/sign_up/domain/repo/sign_up_repo_domain.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignUpRepoImplementaion extends SignUpRepoDomain {
-  final SignUpApiService signUpService;
+  // final SignUpApiService signUpService;
+  final FirebaseAuth signUpService;
 
   SignUpRepoImplementaion({required this.signUpService});
 
@@ -16,11 +17,10 @@ class SignUpRepoImplementaion extends SignUpRepoDomain {
     SignUpRequestBodyEntity signUpRequestBodyEntity,
   ) async {
     try {
-      final response = await signUpService.firebaseAuth!
-          .createUserWithEmailAndPassword(
-            email: signUpRequestBodyEntity.email.toString(),
-            password: signUpRequestBodyEntity.password.toString(),
-          );
+      final response = await signUpService.createUserWithEmailAndPassword(
+        email: signUpRequestBodyEntity.email.toString(),
+        password: signUpRequestBodyEntity.password.toString(),
+      );
 
       if (response.user != null) {
         response.user!.sendEmailVerification();
