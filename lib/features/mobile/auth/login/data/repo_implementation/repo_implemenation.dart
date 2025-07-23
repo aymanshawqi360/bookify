725e33c8494560eac61156eec5c9b5fcad2a1450
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-23 22:51:27 +0300

    added amendment in repo_implemenation

--- lib/features/mobile/auth/login/data/repo_implementation/repo_implemenation.dart
+++ lib/features/mobile/auth/login/data/repo_implementation/repo_implemenation.dart
@@ -1,13 +1,14 @@
 import 'package:book_rental_app/core/errors/api_error_hundler.dart';
 import 'package:book_rental_app/core/errors/api_resulte.dart';
-import 'package:book_rental_app/features/mobile/auth/login/data/api/login_api_service.dart';
 import 'package:book_rental_app/features/mobile/auth/login/data/mapper/login_mapper.dart';
 import 'package:book_rental_app/features/mobile/auth/login/data/model/login_request_model.dart';
 import 'package:book_rental_app/features/mobile/auth/login/domain/entity/login_response_model_entity.dart';
 import 'package:book_rental_app/features/mobile/auth/login/domain/repo/login_repo_domain.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RepoImplemenation extends LoginRepoDomain {
-  final LoginApiService apiFactory;
+  // final LoginApiService apiFactory;
+  final FirebaseAuth apiFactory;
 
   RepoImplemenation({required this.apiFactory});
 
@@ -16,11 +17,10 @@ class RepoImplemenation extends LoginRepoDomain {
     LoginRequestModel loginRequestModel,
   ) async {
     try {
-      final response = await apiFactory.firebaseAuth!
-          .signInWithEmailAndPassword(
-            email: loginRequestModel.email.toString(),
-            password: loginRequestModel.password.toString(),
-          );
+      final response = await apiFactory.signInWithEmailAndPassword(
+        email: loginRequestModel.email.toString(),
+        password: loginRequestModel.password.toString(),
+      );
 
       return ApiResulte.success(
         LoginMapper.getLoginResponseModelEntity(
