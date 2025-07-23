823a35bad242d3660bd5d74d4c0810912667a542
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-23 22:44:14 +0300

    added amendment in app_assest

--- lib/core/config/app_assest.dart
+++ lib/core/config/app_assest.dart
@@ -1,4 +1,6 @@
 class AppAssest {
   static const _assetsSvg = "assets/svg";
   static const svgLogo = "$_assetsSvg/logo.svg";
+  static const svgNotification = "$_assetsSvg/notification.svg";
+  static const search = "$_assetsSvg/search.svg";
 }
