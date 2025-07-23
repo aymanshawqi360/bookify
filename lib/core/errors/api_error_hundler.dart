072cbc520a3cc61636dedda31d743cd8de1ea4d4
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-23 22:45:38 +0300

    added amendment in api_error_hundler

--- lib/core/errors/api_error_hundler.dart
+++ lib/core/errors/api_error_hundler.dart
@@ -1,8 +1,62 @@
 import 'package:book_rental_app/core/errors/api_error_model.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 
 class ApiErrorHundler {
+  static ApiErrorModel apiHundle(dynamic e) {
+    if (e is DioException) {
+      switch (e.type) {
+        case DioExceptionType.cancel:
+          return ApiErrorModel(
+            errorMessage: "The request was cancelled before being sent.",
+          );
+
+        case DioExceptionType.connectionTimeout:
+          return ApiErrorModel(
+            errorMessage:
+                "Connection to the server timed out. Please try again later.",
+          );
+
+        case DioExceptionType.sendTimeout:
+          return ApiErrorModel(
+            errorMessage: "Sending data took too long. Please try again.",
+          );
+
+        case DioExceptionType.receiveTimeout:
+          return ApiErrorModel(
+            errorMessage: "The app couldn't receive data in time.",
+          );
+
+        case DioExceptionType.badCertificate:
+          return ApiErrorModel(
+            errorMessage:
+                "There is a problem with the server's security certificate.",
+          );
+
+        case DioExceptionType.badResponse:
+          return errorHundle(e.response?.data);
+
+        case DioExceptionType.connectionError:
+          return ApiErrorModel(
+            errorMessage:
+                "Please check your internet connection and try again.",
+          );
+
+        case DioExceptionType.unknown:
+          return ApiErrorModel(
+            errorMessage: "An unexpected error occurred. Please try again.",
+          );
+      }
+    } else {
+      return ApiErrorModel(errorMessage: "Unknown error occurred");
+    }
+  }
+
+  static ApiErrorModel errorHundle(dynamic data) {
+    return ApiErrorModel(errorMessage: data.toString());
+  }
+
   static ApiErrorModel handle(dynamic e) {
     if (e is Exception) {
       if (e is FirebaseAuthException) {
